d32a69282bc730d50dc33ba60d76fc1e65588c06
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2023-04-20 11:01:43 +0900

    완

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input-container">
+          <input
+            type="text"
+            value={input}
+            onChange={handleChange}
+            placeholder=""
+          />
+          <button onClick={addTodo}>추가하기</button>
+        </div>
+        <section>
+          <h2 style={{ textAlign: 'center' }}>Todo List</h2>
+          <div className="todo-grid">
+            {todos.map((todo, index) => (
+              <div key={index} className="todo-item">
+                {todo}
+              </div>
+            ))}
+          </div>
+        </section>
       </header>
     </div>
   );
